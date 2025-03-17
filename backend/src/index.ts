7ef0136b93bd78930c30d3d1dd0aa2e15e4655a9
Dusan <dusan.mikic15@gmail.com>
2025-03-17 15:21:49 +0100

    initial backend project setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,15 @@
-import { env } from '@/common/utils/envConfig';
-import { app, logger } from '@/server';
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
 
-const server = app.listen(env.PORT, () => {
-  const { NODE_ENV, HOST, PORT } = env;
-  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3002;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
 });
 
-const onCloseSignal = () => {
-  logger.info('sigint received, shutting down');
-  server.close(() => {
-    logger.info('server closed');
-    process.exit();
-  });
-  setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
-};
-
-process.on('SIGINT', onCloseSignal);
-process.on('SIGTERM', onCloseSignal);
+app.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
