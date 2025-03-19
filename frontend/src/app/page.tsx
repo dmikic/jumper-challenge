d71d77f5419890f4d4bff15ad8a9ec162c62c884
Dusan <dusan.mikic15@gmail.com>
2025-03-19 22:47:27 +0100

    minor cleanup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import Dashboard from "./dashboard/page";
-import Leaderboard from "./leaderboard/page";
 
 export default function Home() {
   return (
     <>
-      <Dashboard /> <Leaderboard />
+      <Dashboard />
     </>
   );
 }
